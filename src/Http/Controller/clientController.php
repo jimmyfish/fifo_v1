485e64b15a95bf6bf654e4e7e043e632b01b7e53
Lutfykumar <lutfhiesita@gmail.com>
2016-12-20 12:53:40 +0700

    edit profil page

--- src/Http/Controller/clientController.php
+++ src/Http/Controller/clientController.php
@@ -367,6 +367,8 @@ class clientController implements ControllerProviderInterface
         return $this->app['twig']->render('Client/profil.twig', ['data' => $data]);
     }
 
+   
+
     public function clientForgetAction(Request $request)
     {
         $session = $this->app['session'];
